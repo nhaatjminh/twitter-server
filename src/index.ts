9e59242afe5325d1de73638011fb80365c961944
nhaatjminh <minhphan.240199@gmail.com>
2023-10-21 16:25:15 +0700

    connect to mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 
 const app = express()
 const port = 3000
+
+databaseService.connect()
 
 app.use(express.json());
 
