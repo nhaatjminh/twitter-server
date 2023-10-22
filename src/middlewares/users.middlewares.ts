99197afd248be769bc1beb8072fbb3ea6d2315d7
nhaatjminh <minhphan.240199@gmail.com>
2023-10-22 17:49:44 +0700

    add validator

--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import { checkSchema } from 'express-validator'
+import { validate } from '~/utils/validation'
 
 export const loginValidator = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body
@@ -9,3 +11,82 @@ export const loginValidator = (req: Request, res: Response, next: NextFunction) 
   }
   next()
 }
+
+export const registerValidator = validate(
+  checkSchema({
+    name: {
+      notEmpty: true,
+      isLength: {
+        options: {
+          min: 1,
+          max: 255
+        }
+      },
+      isString: true,
+      trim: true
+    },
+    email: {
+      notEmpty: true,
+      isEmail: true,
+      trim: true
+    },
+    password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      errorMessage:
+        'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol'
+    },
+    confirm_password: {
+      notEmpty: true,
+      isString: true,
+      isLength: {
+        options: {
+          min: 6,
+          max: 50
+        }
+      },
+      isStrongPassword: {
+        options: {
+          minLength: 6,
+          minLowercase: 1,
+          minUppercase: 1,
+          minNumbers: 1,
+          minSymbols: 1
+        }
+      },
+      custom: {
+        options: (value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Password comfirmation does not match password')
+          }
+          return true
+        }
+      },
+      errorMessage:
+        'Password must be at least 6 characters long and contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol'
+    },
+    date_of_birth: {
+      isISO8601: {
+        options: {
+          strict: true,
+          strictSeparator: true
+        }
+      }
+    }
+  })
+)
