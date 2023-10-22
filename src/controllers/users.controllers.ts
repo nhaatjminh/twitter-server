ec0607a819ecb210307df7c537f8f82140c0082c
nhaatjminh <minhphan.240199@gmail.com>
2023-10-22 19:26:26 +0700

    create access token & refresh token

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express'
-import User from '~/models/schemas/User.schema'
-import databaseService from '~/services/database.services'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { RegisterReqBody } from '~/models/requests/User.requests'
 import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   res.json({ message: 'Login success' })
 }
 
-export const registerController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
+export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
   try {
-    const result = await usersService.register({ email, password })
+    const result = await usersService.register(req.body)
 
-    res.json({ message: 'Register success' })
+    res.json({ message: 'Register success', result })
   } catch (error) {
     res.status(400).json({
       error: 'Register fail'
