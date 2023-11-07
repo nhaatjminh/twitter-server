c461254f05f75d1c3a2853c4e8151cdfd925d179
nhaatjminh <minhphan.240199@gmail.com>
2023-11-07 16:57:12 +0700

    auto create folder

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,14 @@ import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediaRouter from './routes/media.routes'
+import { initFolder } from './utils/files'
 
 databaseService.connect()
 
 const app = express()
 const port = 5000
+
+initFolder()
 
 app.use(express.json())
 
